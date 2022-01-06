995dbdaf5578d2d30421d3b9703a0e51e7a3da5b
Tyler Caceres <tcaceres@connected.io>
2022-01-06 10:15:47 -0500

    clean up routes

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -1,3 +1,17 @@
 class TrainingSession < ApplicationRecord
   belongs_to :skill
+
+   NEXT_LEVEL = {
+    'beginner' => 'intermediate',
+    'intermediate' => 'advanced',
+    'advanced' => 'advanced',
+  }
+
+  after_create :increment_skill_level!
+
+  private
+
+  def increment_skill_level!
+    skill.update!(level: NEXT_LEVEL[skill.level])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :training_sessions
-  resources :skills
+  resources :training_sessions, except: [:update, :destroy]
+  resources :skills, except: [:update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
