3ab0f005663c77efc0daba2691fb4fe455725f33
Tyler Caceres <tcaceres@connected.io>
2022-01-06 09:38:28 -0500

    add validation and enum to the skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,7 @@
 class Skill < ApplicationRecord
+    enum level: {beginner: 0, intermediate: 1, advanced: 2}
+    validates :name,
+        presence: true,
+        uniqueness: true,
+        format: {with: /\A[a-z\-]*\z/, message: "'%{value}' must be in kebab-case"}
 end
